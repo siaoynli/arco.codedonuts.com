9895659624862005f8c75c7b47dd187216a1677b
siaoynli <siaoynli@126.com>
2022-07-07 14:34:38 +0800

    add comment

--- app/Utils/helper.php
+++ app/Utils/helper.php
@@ -1,19 +1,40 @@
 <?php
-declare (strict_types = 1);
+/*
+ * @Author     : lixiaoyun
+ * @Email      : 120235331@qq.com
+ * @Github     : http://www.github.com/siaoynli
+ * @Date       :  2021/7/7 14:18
+ * @Description:  辅助函数文件
+ * @Copyright (c) 2022 http://www.hangzhou.com.cn All rights reserved.
+ */
+
+declare (strict_types=1);
+
+/**
+ * @Author: lixiaoyun
+ * @Email: 120235331@qq.com
+ * @Date: 2022/7/7 14:18
+ * @Description: 返回响应信息
+ * @return string
+ */
+function ping(): string
+{
+    return "it's ok!";
+}
 
 
 /**
  * @Author: lixiaoyun
  * @Email: 120235331@qq.com
- * @Date: 2019/6/27 17:14
- * @Description:数据加密解密
+ * @Date: 2022/7/7 14:18
+ * @Description: 数据加密解密
  * @param $string
  * @param string $operation
  * @param string $key
  * @param int $expiry
  * @return string
  */
-function sys_auth($string, string $operation = 'ENCODE', string $key = '', int $expiry = 0):string
+function sys_auth($string, string $operation = 'ENCODE', string $key = '', int $expiry = 0): string
 {
     $string = trim($string);
     if ($operation == 'ENCODE') {
@@ -28,7 +49,7 @@ function sys_auth($string, string $operation = 'ENCODE', string $key = '', int $
     $keys = md5(substr($runtokey, 0, 16) . substr($fixedkey, 0, 16) . substr($runtokey, 16) . substr($fixedkey, 16));
     $string = $operation == 'ENCODE' ? sprintf('%010d', $expiry ? $expiry + time() : 0) . substr(md5($string . $egiskeys), 0, 16) . $string : base64_decode(substr($string, $key_length));
 
-    $i = 0;
+
     $result = '';
     $string_length = strlen($string);
     for ($i = 0; $i < $string_length; $i++) {
