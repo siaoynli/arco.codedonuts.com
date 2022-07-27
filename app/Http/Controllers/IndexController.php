2c9b15ed6e153823cfce8abd8e7d174e91e2c20c
siaoynli <siaoynli@126.com>
2022-07-27 11:22:14 +0800

    添加自动赋值功能

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
-use App\Models\Api\V1\Article;
 use App\Models\Api\V1\User;
 
 class IndexController extends Controller
@@ -15,18 +14,6 @@ class IndexController extends Controller
 
     public function user()
     {
-        $article = [
-            "title" => "hello world",
-            "summary" => "sdfdsf",
-            "content" => "Sdfsdf",
-            "author" => "Sdfsdf",
-            "editor" => "Sdfsdf",
-
-        ];
-
-        Article::create($article);
-
-
         return new UserResource(User::find(1));
     }
 }
