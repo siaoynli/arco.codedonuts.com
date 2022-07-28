1cd4f527530e40336488991b18bf747f4bf90a9e
siaoynli <siaoynli@126.com>
2022-07-28 10:05:08 +0800

    修复同步索引bug

--- app/Console/Commands/Dev/ESMigrate.php
+++ app/Console/Commands/Dev/ESMigrate.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands\Dev;
 
 use App\Console\Commands\Dev\Indexs\ProjectIndex;
+use Exception;
 use Illuminate\Console\Command;
+
 
 class ESMigrate extends Command
 {
@@ -11,8 +13,7 @@ class ESMigrate extends Command
     protected $signature = 'es:migrate';
     protected $description = 'Elasticsearch 索引结构迁移';
 
-    protected $es;
-
+    protected mixed $es;
 
     public function handle()
     {
@@ -37,8 +38,7 @@ class ESMigrate extends Command
             try {
                 $this->info('索引存在，准备更新');
                 $this->updateIndex($aliasName, $indexClass);
-            } catch (\Exception $e) {
-
+            } catch (Exception) {
                 $this->warn('更新失败，准备重建');
                 $this->reCreateIndex($aliasName, $indexClass);
             }
@@ -47,8 +47,16 @@ class ESMigrate extends Command
     }
 
 
-    // 创建新索引
-    protected function createIndex($aliasName, $indexClass)
+    /**
+     * @Author: lixiaoyun
+     * @Email: 120235331@qq.com
+     * @Date: 2022/7/28 10:01
+     * @Description: 创建新索引
+     * @param $aliasName
+     * @param $indexClass
+     * @return void
+     */
+    protected function createIndex($aliasName, $indexClass): void
     {
         // 调用 create() 方法创建索引
         $this->es->indices()->create([
@@ -70,8 +78,16 @@ class ESMigrate extends Command
     }
 
 
-    // 更新已有索引
-    protected function updateIndex($aliasName, $indexClass)
+    /**
+     * @Author: lixiaoyun
+     * @Email: 120235331@qq.com
+     * @Date: 2022/7/28 10:00
+     * @Description: 更新已有索引
+     * @param $aliasName
+     * @param $indexClass
+     * @return void
+     */
+    protected function updateIndex($aliasName, $indexClass): void
     {
         // 暂时关闭索引
         $this->es->indices()->close(['index' => $aliasName]);
@@ -95,18 +111,26 @@ class ESMigrate extends Command
         $this->es->indices()->open(['index' => $aliasName]);
     }
 
-    // 重建索引
-    protected function reCreateIndex($aliasName, $indexClass)
+    /**
+     * @Author: lixiaoyun
+     * @Email: 120235331@qq.com
+     * @Date: 2022/7/28 10:00
+     * @Description:
+     * @param $aliasName
+     * @param $indexClass
+     * @return void
+     */
+    protected function reCreateIndex($aliasName, $indexClass): void
     {
         // 获取索引信息，返回结构的 key 为索引名称，value 为别名
         $indexInfo = $this->es->indices()->getAliases(['index' => $aliasName]);
         // 取出第一个 key 即为索引名称
-        $indexName = array_keys($indexInfo)[0];
+        $oldIndexName = array_keys($indexInfo)[0];
         // 用正则判断索引名称是否以 _数字 结尾
-        if (!preg_match('~_(\d+)$~', $indexName, $m)) {
-            $msg = '索引名称不正确:' . $indexName;
+        if (!preg_match('~_(\d+)$~', $oldIndexName, $m)) {
+            $msg = '索引名称不正确:' . $oldIndexName;
             $this->error($msg);
-            throw new \Exception($msg);
+            return;
         }
         // 新的索引名称
         $newIndexName = $aliasName . '_' . ($m[1] + 1);
@@ -125,7 +149,7 @@ class ESMigrate extends Command
         $this->info('重建成功，准备修改别名');
         $this->es->indices()->putAlias(['index' => $newIndexName, 'name' => $aliasName]);
         $this->info('修改成功，准备删除旧索引');
-        $this->es->indices()->delete(['index' => $indexName]);
+        $this->es->indices()->delete(['index' => $oldIndexName]);
         $this->info('删除成功');
     }
 }
