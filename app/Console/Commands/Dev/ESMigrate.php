e8c7e143cc1f7a7fe68e95492add03f597dd98b6
siaoynli <siaoynli@126.com>
2022-07-29 17:25:15 +0800

    es迁移到8版本

--- app/Console/Commands/Dev/ESMigrate.php
+++ app/Console/Commands/Dev/ESMigrate.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands\Dev;
 
 use App\Console\Commands\Dev\Indexs\ProjectIndex;
+use Elastic\Elasticsearch\Response\Elasticsearch;
 use Exception;
+use Http\Promise\Promise;
 use Illuminate\Console\Command;
 
 
@@ -14,11 +16,11 @@ class ESMigrate extends Command
     protected $description = 'Elasticsearch 索引结构迁移';
 
     protected mixed $es;
+    protected Elasticsearch|Promise $res;
 
     public function handle()
     {
         $this->es = app('es');
-
         // 索引类数组，先留空
         $indices = [ProjectIndex::class];
         // 遍历索引类数组
@@ -27,11 +29,14 @@ class ESMigrate extends Command
             $aliasName = $indexClass::getAliasName();
 
             $this->info('正在处理索引 ' . $aliasName);
+
             // 通过 exists 方法判断这个别名是否存在
             if (!$this->es->indices()->exists(['index' => $aliasName])->asBool()) {
                 $this->info('索引不存在，准备创建');
-                $this->createIndex($aliasName, $indexClass);
-                $this->info('创建成功，准备初始化数据');
+                $this->res = $this->createIndex($aliasName, $indexClass);
+                if ($this->res->asBool()) {
+                    $this->info('创建成功，准备初始化数据');
+                }
                 $indexClass::rebuild($aliasName);
                 $this->info('操作成功');
                 continue;
@@ -56,12 +61,12 @@ class ESMigrate extends Command
      * @Description: 创建新索引
      * @param $aliasName
      * @param $indexClass
-     * @return void
+     * @return Elasticsearch|Promise
      */
-    protected function createIndex($aliasName, $indexClass): void
+    protected function createIndex($aliasName, $indexClass): Elasticsearch|Promise
     {
         // 调用 create() 方法创建索引
-        $this->es->indices()->create([
+        return $this->es->indices()->create([
             // 第一个版本的索引名后缀为 _0
             'index' => $aliasName . '_0',
             'body' => [
@@ -91,15 +96,22 @@ class ESMigrate extends Command
      */
     protected function updateIndex($aliasName, $indexClass): void
     {
+
         // 暂时关闭索引
-        $this->es->indices()->close(['index' => $aliasName]);
+        $this->res = $this->es->indices()->close(['index' => $aliasName]);
+        if ($this->res->asBool()) {
+            $this->info('关闭索引' . $aliasName . ',ok');
+        }
         // 更新索引设置
-        $this->es->indices()->putSettings([
+        $this->res = $this->es->indices()->putSettings([
             'index' => $aliasName,
             'body' => $indexClass::getSettings(),
         ]);
+        if ($this->res->asBool()) {
+            $this->info('更新索引设置,ok!');
+        }
         // 更新索引字段
-        $this->es->indices()->putMapping([
+        $this->res = $this->es->indices()->putMapping([
             'index' => $aliasName,
             'body' => [
                 '_source' => [
@@ -109,8 +121,14 @@ class ESMigrate extends Command
                 'properties' => $indexClass::getProperties(),
             ],
         ]);
+        if ($this->res->asBool()) {
+            $this->info('更新索引字段,ok!');
+        }
         // 重新打开索引
-        $this->es->indices()->open(['index' => $aliasName]);
+        $this->res = $this->es->indices()->open(['index' => $aliasName]);
+        if ($this->res->asBool()) {
+            $this->info('重新打开索引' . $aliasName . ',ok!');
+        }
     }
 
     /**
@@ -124,10 +142,14 @@ class ESMigrate extends Command
      */
     protected function reCreateIndex($aliasName, $indexClass): void
     {
-
-
         // 获取索引信息，返回结构的 key 为索引名称，value 为别名
-        $indexInfo = $this->es->indices()->getAlias(['index' => $aliasName]);
+        $this->res = $this->es->indices()->getAlias(['index' => $aliasName]);
+        if (!$this->res->asBool()) {
+            $msg = '索引别名没有获取到:' . $aliasName;
+            $this->error($msg);
+            return;
+        }
+        $indexInfo = $this->res->asArray();
         // 取出第一个 key 即为索引名称
         $oldIndexName = array_keys($indexInfo)[0];
         // 用正则判断索引名称是否以 _数字 结尾
@@ -139,7 +161,7 @@ class ESMigrate extends Command
         // 新的索引名称
         $newIndexName = $aliasName . '_' . ($m[1] + 1);
         $this->info('正在创建索引' . $newIndexName);
-        $this->es->indices()->create([
+        $this->res = $this->es->indices()->create([
             'index' => $newIndexName,
             'body' => [
                 'settings' => $indexClass::getSettings(),
@@ -148,12 +170,18 @@ class ESMigrate extends Command
                 ],
             ],
         ]);
-        $this->info('创建成功，准备重建数据');
+        if ($this->res->asBool()) {
+            $this->info('创建成功，准备重建数据');
+        }
         $indexClass::rebuild($newIndexName);
         $this->info('重建成功，准备修改别名');
-        $this->es->indices()->putAlias(['index' => $newIndexName, 'name' => $aliasName]);
-        $this->info('修改成功，准备删除旧索引');
-        $this->es->indices()->delete(['index' => $oldIndexName]);
-        $this->info('删除成功');
+        $this->res = $this->es->indices()->putAlias(['index' => $newIndexName, 'name' => $aliasName]);
+        if ($this->res->asBool()) {
+            $this->info('修改成功，准备删除旧索引');
+        }
+        $this->res = $this->es->indices()->delete(['index' => $oldIndexName]);
+        if ($this->res->asBool()) {
+            $this->info('删除成功');
+        }
     }
 }
